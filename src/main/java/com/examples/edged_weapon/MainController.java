1e0f18cd566af52974fa98ef707f70d674b113fb
pabquamul <mr.dmitriy.yugay.ru@mail.ru>
2020-10-06 19:33:15 +0400

    let's start

---
+++ src/main/java/com/examples/edged_weapon/MainController.java
@@ -0,0 +1,18 @@
+package com.examples.edged_weapon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/mainMenu")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "mainMenu";
+    }
+
+}
+
